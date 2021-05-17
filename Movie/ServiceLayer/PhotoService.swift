5a10b3c556131ebf06b20d32df90374ab4db973f
DenysProg <denysnikolaichuk@gmail.com>
2021-05-17 20:48:20 +0300

    Created saveImageToCatch and getImageFromCache

--- Movie/ServiceLayer/PhotoService.swift
+++ Movie/ServiceLayer/PhotoService.swift
@@ -34,12 +34,33 @@ final class PhotoService: PhotoServiceProtocol {
         return pathName
     }()
     
-    func saveImageToCatch(urlString: String, image: UIImage) {
-        <#code#>
+    private func getFilePath(urlString: String) -> String? {
+        guard let cacheDir = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first
+        else { return nil }
+
+        let hashName = String(describing: urlString.hashValue)
+        return cacheDir.appendingPathComponent(PhotoService.pathName + "/" + hashName).path
     }
-    
+
+    func saveImageToCatch(urlString: String, image: UIImage) {
+        guard let filename = getFilePath(urlString: urlString) else { return }
+
+        let data = image.pngData()
+        FileManager.default.createFile(atPath: filename, contents: data, attributes: nil)
+    }
+
     func getImageFromCache(urlString: String) -> UIImage? {
-        <#code#>
+        guard let filename = getFilePath(urlString: urlString),
+              let info = try? FileManager.default.attributesOfItem(atPath: filename),
+              let modificationDate = info[FileAttributeKey.modificationDate] as? Date else { return nil }
+        let lifetime = Date().timeIntervalSince(modificationDate)
+
+        guard lifetime <= cacheLifeTime,
+              let image = UIImage(contentsOfFile: filename) else { return nil }
+
+        images[urlString] = image
+
+        return image
     }
     
     func loadPhoto(by urlString: String, completion: @escaping (UIImage?) -> ()) {
