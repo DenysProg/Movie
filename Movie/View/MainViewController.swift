ea8084e63b932a0d1119741b970945903d94bc68
DenysProg <denysnikolaichuk@gmail.com>
2021-05-17 18:31:24 +0300

    Set up vc to get data

--- Movie/View/MainViewController.swift
+++ Movie/View/MainViewController.swift
@@ -7,10 +7,36 @@
 
 import UIKit
 
+import CoreData
+import UIKit
+
 ///
-final class ViewController: UIViewController {
+final class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    @IBOutlet var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        setNavigationBar()
+        loadData()
+    }
+
+    private func loadData() {
+    
+    }
+
+    private func setNavigationBar() {
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.topItem?.title = "Movies"
     }
 }
