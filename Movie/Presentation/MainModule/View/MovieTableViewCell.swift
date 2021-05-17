41d8401b90f53b65f8e0b2d9199eef5f5170e0d1
DenysProg <denysnikolaichuk@gmail.com>
2021-05-17 21:00:53 +0300

    Configure MainViewCell delegate to show data

--- Movie/Presentation/MainModule/View/MovieTableViewCell.swift
+++ Movie/Presentation/MainModule/View/MovieTableViewCell.swift
@@ -7,12 +7,26 @@
 
 import UIKit
 
-class MovieTableViewCell: UITableViewCell {
+protocol MovieTableViewCellDelegate: AnyObject {
+    func getImage(byPath: String, completion: @escaping (UIImage) -> ())
+}
+
+///
+final class MovieTableViewCell: UITableViewCell {
     @IBOutlet private var titleLable: UILabel!
     @IBOutlet private var movieImage: UIImageView!
     @IBOutlet private var descriptLabel: UILabel!
     @IBOutlet private var rateMovieLabel: UILabel!
     
+    weak var delegate: MovieTableViewCellDelegate?
+
+    func setCellWithValuesOf(_ movie: ResultMovie) {
+        updateUI(title: movie.title, rate: "\(movie.rate ?? 0)", overview: movie.overview, image: movie.backdropImage)
+        delegate?.getImage(byPath: movie.backdropImage ?? "") { [weak self] image in
+            self?.movieImage.image = image
+        }
+        viewsAttributes()
+    }
    
     private func updateUI(
         title: String?,
