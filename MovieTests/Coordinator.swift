d2186d0ed2071ff24c62885118b3f231da3ab756
DenysProg <denysnikolaichuk@gmail.com>
2021-05-17 23:10:11 +0300

    Wrote unit test coordinator

---
+++ MovieTests/Coordinator.swift
@@ -0,0 +1,40 @@
+//
+//  Coordinator.swift
+//  MovieTests
+//
+//  Created by Denys Nikolaichuk on 17.05.2021.
+//
+
+@testable import Movie
+import XCTest
+
+///
+final class MockNavigationController: UINavigationController {
+    var presentedVC: UIViewController?
+
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        presentedVC = viewController
+        super.pushViewController(viewController, animated: animated)
+    }
+}
+
+///
+final class CoordinatorTest: XCTestCase {
+    var coordinator: MainCoordinator!
+    var navigationController = MockNavigationController()
+    var assemblyBuilder = AssemblerModuleBuilder()
+
+    override func setUpWithError() throws {
+        coordinator = MainCoordinator(navigationController: navigationController, assemblyBuilder: assemblyBuilder)
+    }
+
+    override func tearDownWithError() throws {
+        coordinator = nil
+    }
+
+    func testRouter() {
+        coordinator.detailSubscription(movie: nil)
+        let detailViewController = navigationController.presentedVC
+        XCTAssertTrue(detailViewController is DetailViewController)
+    }
+}
