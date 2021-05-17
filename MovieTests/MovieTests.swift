691eb301b55a78ade4802ed837bc17528355c11c
DenysProg <denysnikolaichuk@gmail.com>
2021-05-17 17:56:55 +0300

    Created Core Data Movie Entity

--- MovieTests/MovieTests.swift
+++ MovieTests/MovieTests.swift
@@ -5,11 +5,11 @@
 //  Created by Denys Nikolaichuk on 17.05.2021.
 //
 
-import XCTest
 @testable import Movie
+import XCTest
 
+///
 class MovieTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -25,9 +25,8 @@ class MovieTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
--- MovieUITests/MovieUITests.swift
+++ MovieUITests/MovieUITests.swift
@@ -7,8 +7,8 @@
 
 import XCTest
 
+///
 class MovieUITests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
